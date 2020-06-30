ba407a1911b6089a89b3a1d3356f823b03cb087d
Yuriy <yura.ganzin@mail.ru>
2020-06-30 12:43:02 +0300

    Добавлена касса и модульные тесты к ней.

---
+++ CrmBL/Model/CashDesk.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrmBL.Model
+{
+    /// <summary>
+    /// Касса.
+    /// </summary>
+    public class CashDesk
+    {
+        CRMContext db = new CRMContext();
+
+        /// <summary>
+        /// Номер кассы.
+        /// </summary>
+        public int Number { get; set; }
+        
+        /// <summary>
+        /// Продавец.
+        /// </summary>
+        public Seller Seller { get; set; }
+        
+        /// <summary>
+        /// Очередь покупателей.
+        /// </summary>
+        public Queue<Cart> Carts { get; set; }
+        
+        /// <summary>
+        /// Максимальное количество покупателей.
+        /// </summary>
+        public int MaxQueueCount { get; set; }
+        
+        /// <summary>
+        /// Ушедшие покупатели из-за нехватки мест на кассе.
+        /// </summary>
+        public int ExitCustomers { get; set; }
+        
+        /// <summary>
+        /// Создана ли касса в целях моделирования.
+        /// </summary>
+        public bool IsModel { get; set; }
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="number"> Номер кассы. </param>
+        /// <param name="seller"> Продавец. </param>
+        /// <param name="maxQueueCount"> Максимальное количество покупателей. </param>
+        /// <param name="isModel"> Создана ли касса в целях моделирования. </param>
+        public CashDesk(int number, Seller seller, int maxQueueCount = 10, bool isModel = true)
+        {
+            Number = number;
+            Seller = seller;
+            MaxQueueCount = maxQueueCount;
+            IsModel = isModel;
+            Carts = new Queue<Cart>();
+        }
+
+        /// <summary>
+        /// Произвести покупку.
+        /// </summary>
+        /// <returns> Итоговая цена. </returns>
+        public decimal Dequeue()
+        {
+            decimal summ = 0;
+            var currCart = Carts.Dequeue();
+            if (currCart != null)
+            {
+                var check = new Check()
+                {
+                    Customer = currCart.Customer,
+                    CustomerId = currCart.Customer.CustomerId,
+                    Seller = Seller,
+                    SellerId = Seller.SellerId,
+                    DataCreated = DateTime.Now
+                };
+
+                if (!IsModel)
+                {
+                    db.Checks.Add(check);
+                }
+                else
+                {
+                    check.CheckId = 0;
+                }
+
+                List<Sell> sells = new List<Sell>();
+                foreach (Product product in currCart)
+                {
+                    if (product.Count == 0)
+                        break;
+
+                    var sell = new Sell()
+                    {
+                        Check = check,
+                        CheckId = check.CheckId,
+                        Product = product,
+                        ProductId = product.ProductId
+                    };
+                    summ += product.Price;
+                    sells.Add(sell);
+
+                    db.Sells.Add(sell);
+                    product.Count--;
+                }
+
+                if (!IsModel)
+                {
+                    db.SaveChanges();
+                }
+            }
+
+            return summ;
+        }
+
+        /// <summary>
+        /// Добавить корзину.
+        /// </summary>
+        /// <param name="cart"> Корзина. </param>
+        public void Enqueue(Cart cart)
+        {
+            if (Carts.Count < MaxQueueCount)
+            {
+                Carts.Enqueue(cart);
+            }
+            else
+            {
+                ExitCustomers++;
+            }
+        }
+    }
+}
---
+++ CrmBLTests/Model/CashDeskTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrmBL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBL.Model.Tests
+{
+    [TestClass()]
+    public class CashDeskTests
+    {
+        [TestMethod()]
+        public void CashDeskTest()
+        {
+            // Arrange
+            Customer customer = new Customer() { Name = "Игорь", CustomerId = 0 };
+            Customer customer2 = new Customer() { Name = "Чел", CustomerId = 1 };
+            Seller seller = new Seller();
+            int number = 0;
+            CashDesk cashDesk = new CashDesk(number, seller);
+
+            Product product1 = new Product()
+            {
+                ProductId = 0,
+                Name = "Клубника",
+                Price = 100,
+                Count = 50
+            };
+            Product product2 = new Product()
+            {
+                ProductId = 1,
+                Name = "Мороженное",
+                Price = 75,
+                Count = 50
+            };
+            Cart cart1 = new Cart(customer);
+            cart1.Add(product2);
+            cart1.Add(product1, 5);
+            Cart cart2 = new Cart(customer2);
+            cart2.Add(product2);
+            cart2.Add(product1);
+            cart2.Add(product1);
+
+            // Act
+            cashDesk.Enqueue(cart1);
+            cashDesk.Enqueue(cart2);
+            var resSumm1 = cashDesk.Dequeue();
+            var resSumm2 = cashDesk.Dequeue();
+
+            // Assert
+            Assert.AreEqual(575, resSumm1);
+            Assert.AreEqual(275, resSumm2);
+            Assert.AreEqual(43, product1.Count);
+            Assert.AreEqual(48, product2.Count);
+            Assert.AreEqual(0, cashDesk.Carts.Count);
+        }
+    }
+}
